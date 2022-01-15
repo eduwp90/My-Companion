8e870ba3542c218091b1be58cd1e62aece9064e2
eduwp90 <eduwp90@gmail.com>
2022-01-15 11:32:00 +0100

    server: changed helmet config to prevent blocking images

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const helmet = require("helmet");
-const Agendash = require("agendash");
+const helmet = require('helmet');
+const Agendash = require('agendash');
 require('dotenv').config();
 
 const router = require('./router.js');
-const { agenda, api, dashboard, removeCSP, ParseInit } = require("./config/config.js");
+const {
+  agenda,
+  api,
+  dashboard,
+  removeCSP,
+  ParseInit,
+} = require('./config/config.js');
 
 ParseInit();
 
@@ -15,18 +21,26 @@ const serverUrl = process.env.SERVER_URL;
 
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
-app.use('/agendash',removeCSP, Agendash(agenda));
+app.use('/agendash', removeCSP, Agendash(agenda));
 
 // Serve the Parse API on the /parse URL prefix
 app.use('/database', api);
-app.use('/dashboard',removeCSP,dashboard);
+app.use('/dashboard', removeCSP, dashboard);
 
 app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Database listening on ${serverUrl}`);
-  console.log(`Dashboard running on ${serverUrl.replace('/database','')}/dashboard`);
-  console.log(`Agendash running on ${serverUrl.replace('/database','')}/agendash`);
+  console.log(
+    `Dashboard running on ${serverUrl.replace('/database', '')}/dashboard`
+  );
+  console.log(
+    `Agendash running on ${serverUrl.replace('/database', '')}/agendash`
+  );
 });
