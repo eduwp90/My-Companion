312f4a8b2967730620b6945b16383eb34608dac7
Jaime D. Rguez-Mastretta <jaimemastretta@Jaimes-MacBook-Pro.local>
2022-01-20 17:20:59 +0100

    feat(Unit test): ErrorMessage renders properly

---
+++ client/src/components/login/errorMessage.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import ErrorMessage from './errorMessage';
+
+describe ('ErrorMessage', () => {
+  test ('ErrorMessage should match snapshot', () => {
+    const { container } = render(<ErrorMessage />);
+    expect (container.firstChild).toMatchSnapshot();
+  })
+})
