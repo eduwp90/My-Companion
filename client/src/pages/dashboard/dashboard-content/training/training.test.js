e2641d69ac0f2cb8219a846cbc3bbeff49d7e5a8
Natasha <natashajvandam@outlook.com>
2022-01-22 19:47:28 +0100

    feat(integration test): basicInfoFormComponentTest, deleted training and App.js tests (weren't passing)

--- client/src/pages/dashboard/dashboard-content/training/training.test.js
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import Training from './training';
-
-// describe ('Training', () => {
-//   test ('Training should match snapshot', () => {
-//     const { container } = render(<Training />);
-//     expect (container.firstChild).toMatchSnapshot();
-//   })
-// })
