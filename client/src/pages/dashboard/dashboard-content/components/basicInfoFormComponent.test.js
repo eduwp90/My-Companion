6fe30ecbf68d3e12f15588cb6b5870a56b4f5e11
Natasha <natashajvandam@outlook.com>
2022-01-24 16:48:42 +0100

    feat(Kostas review notes): change name of form labels in basicInfoFormComponent test

--- client/src/pages/dashboard/dashboard-content/components/basicInfoFormComponent.test.js
+++ client/src/pages/dashboard/dashboard-content/components/basicInfoFormComponent.test.js
@@ -39,10 +39,10 @@ describe ('BasicInfoFormComponent', () => {
   })
   test('should not render form labels if formLable set to false', () => {
     render(<BasicInfoFormComponent parent={filledIn} handleChange={handleChange} formLabel={false} size={"sm"} Data={Data} editable={true} />)
-    const label1 = screen.queryByText(/Pet name/)
-    const label2 = screen.queryByText(/Breed/)
-    expect(label1).not.toBeInTheDocument()
-    expect(label2).not.toBeInTheDocument()
+    const formLabel_Name = screen.queryByText(/Pet name/)
+    const formLabel_Breed = screen.queryByText(/Breed/)
+    expect(formLabel_Name).not.toBeInTheDocument()
+    expect(formLabel_Breed).not.toBeInTheDocument()
   })
 
   test('form should render correctly when formLabel set to true', () => {
